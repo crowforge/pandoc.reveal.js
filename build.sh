acdd204b9e62484a8e28f4f964feaa7f48bb9da7
Simon Holywell <treffynnon@php.net>
2014-01-26 21:50:01 +0000

    Add sed line to enable slide numbers

--- build.sh
+++ build.sh
@@ -4,3 +4,5 @@ git submodule init
 git submodule update
 cp src/reveal.js output/ -r
 ~/.cabal/bin/pandoc -t revealjs --highlight-style=espresso -H src/additional_header.html --variable theme="default" --variable transition="fade" -s src/slides.md -o output/index.html
+
+sed -i "s|Reveal.initialize({|Reveal.initialize({ slideNumber: true, |g" output/index.html
